e806cc169ec32f8bb436ac951859d753f89aa266
Jekaexe <117809005+Jekaexe@users.noreply.github.com>
2022-12-02 16:34:26 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,14 @@
+﻿Program naconecto;
+uses graphabc;
+BEGIN
+setpencolor(clrandom);
+circle(50,200,10);
+circle(80,200,10);
+circle(110,200,10);
+circle(140,200,10);
+circle(170,200,10);
+circle(200,200,10);
+circle(230,200,10);
+circle(260,200,10);
+circle(290,200,10);
+END.
---
+++ 4.pas
@@ -0,0 +1,23 @@
+﻿Program naconecto;
+uses graphabc;
+BEGIN
+randomize;
+var c:integer;
+c:=1;
+repeat
+setpencolor(clrandom);
+circle(100,100,random(1000));
+circle(100,100,random(99));
+circle(100,100,random(97));
+circle(100,100,random(90));
+circle(100,100,random(80));
+circle(100,100,random(70));
+circle(100,100,random(60));
+circle(100,100,random(55));
+circle(100,100,random(50));
+circle(100,100,random(40));
+circle(100,100,random(30));
+circle(100,100,random(20));
+c+=1;
+until c = 300000
+END.
